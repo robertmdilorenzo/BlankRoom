383983dfcb47750947a087f134e862c4ec8c9704
robertmdilorenzo <robertmdilorenzo@gmail.com>
2019-02-10 17:27:26 -0500

    Pen Write
    Very rough pen writing using a trail  renderer. Writes in the air, not on walls yet

---
+++ BlankRoomUnity/Assets/PenTool.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PenTool : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+         
+
+    }
+}
