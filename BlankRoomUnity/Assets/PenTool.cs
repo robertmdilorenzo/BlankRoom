492edd54a84580be2152f8fb592dcd06e9e7d7af
robertmdilorenzo <robertmdilorenzo@gmail.com>
2019-02-10 19:13:20 -0500

    Writing Tweaks
    Working with the offset to get proper pen tracking

--- BlankRoomUnity/Assets/PenTool.cs
+++ BlankRoomUnity/Assets/PenTool.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 public class PenTool : MonoBehaviour
 {
     // Start is called before the first frame update
-    
+    public float mouseSensitivity;
+
+    private Vector3 offset;
     void Start()
     {
-        
-        
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f));
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.position = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        gameObject.transform.position = Camera.main.ScreenToViewportPoint(Input.mousePosition) + offset;
          
 
     }
