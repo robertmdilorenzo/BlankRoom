05bd49c328e540723226ee3f10d4ecebb531cdeb
robertmdilorenzo <robertmdilorenzo@gmail.com>
2019-02-19 13:28:14 -0500

    Basic wall writing implemented (does not track properly yet)
    -User can write on a wall and have it stay there
    -Still need to have it track poperly to the mouse for the render target
    -Need to tweak on click and mouse movement for the render pen so it looks like it lines up properly with the real pen

--- BlankRoomUnity/Assets/PenAlternate.cs
+++ BlankRoomUnity/Assets/PenAlternate.cs
@@ -7,16 +7,29 @@ public class PenAlternate : MonoBehaviour
     // Start is called before the first frame update
     float distance = 3;
     ParticleSystem myTrail;
+    public Transform uvLocation;
+    Vector3 uvWorldPosition;
+    public GameObject renderPen;
+    ParticleSystem renderPenParticles;
     void Start()
     {
+        
+        uvWorldPosition = Vector3.zero;
         myTrail = gameObject.GetComponent<ParticleSystem>();
-        myTrail.Stop();
+      
+        renderPenParticles = renderPen.GetComponent<ParticleSystem>();
+        //myTrail.Stop();
+        //renderPenParticles.Stop();
     }
 
     // Update is called once per frame
     void Update()
     {
         ObjectFollowCursor();
+        if (Input.GetMouseButton(0))
+        {
+            DoAction();
+        }
     }
 
     private void ObjectFollowCursor()
@@ -26,13 +39,53 @@ public class PenAlternate : MonoBehaviour
         gameObject.transform.position = point;
     }
 
-    private void OnMouseDown()
+ 
+
+    void DoAction()
     {
-        myTrail.Play();
+        if (HitTestUVPosition(ref uvWorldPosition))
+        {
+            //Debug.LogWarning("Hit successful");
+
+            //myTrail.emissionRate = 25;
+            //Debug.Log(myTrail.isPlaying);
+            //renderPenParticles.emissionRate = 25;
+            //GameObject.Find("RenderPen").transform.position = uvWorldPosition;
+
+        } else
+        {
+            Debug.LogError("Hit failed");
+        }
     }
 
-    private void OnMouseUp()
+
+    bool HitTestUVPosition(ref Vector3 uvWorldPosition)
     {
-        myTrail.Stop();
+        RaycastHit hit;
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 cursorPos = new Vector3(mousePos.x, mousePos.y, 0.0f);
+
+        Ray cursorRay = Camera.main.ScreenPointToRay(cursorPos);
+        if(Physics.Raycast(cursorRay, out hit, 1000))
+        {
+            MeshCollider meshCollider = hit.collider as MeshCollider;
+            if(meshCollider == null || meshCollider.sharedMesh == null)
+            {
+                return false;
+            }
+            Vector2 pixelUV = new Vector2(hit.textureCoord.x, hit.textureCoord.y);
+            //Debug.Log(pixelUV);
+            uvWorldPosition.x = pixelUV.x;
+            uvWorldPosition.y = pixelUV.y;
+            uvWorldPosition.z = -1.0f;
+            return true;
+
+        }
+        else
+        {
+            return false;
+        }
     }
+
+
 }
---
+++ BlankRoomUnity/Assets/WallWriting.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallWriting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public RenderTexture canvasTexture;
+    public Material baseMaterial;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            MergeTexture();
+        }
+    }
+    private void OnMouseDown()
+    {
+        MergeTexture();
+    }
+    private void OnMouseUp()
+    {
+        MergeTexture();
+    }
+
+    void MergeTexture()
+    {
+        Debug.Log("Merging...");
+        RenderTexture.active = canvasTexture;
+        int width = canvasTexture.width;
+        int height = canvasTexture.height;
+
+        Texture2D tex = new Texture2D(width, height, TextureFormat.RGB24, false);
+        tex.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+        tex.Apply();
+        RenderTexture.active = null;
+        baseMaterial.mainTexture = tex;
+        Debug.Log("Merge Complete!");
+    }
+
+    void SaveTexture()
+    {
+
+    }
+}
