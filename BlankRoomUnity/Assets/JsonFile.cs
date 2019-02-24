b7942041522c7ca15699665b6d0b1561c3d8d89b
Karl Lamoureux <lamoureuxk@wit.edu>
2019-02-24 13:26:08 -0500

    Created SaveImage

--- BlankRoomUnity/Assets/JsonFile.cs
+++ BlankRoomUnity/Assets/JsonFile.cs
@@ -1,7 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Drawing;
 
 //https://unity3d.com/learn/tutorials/topics/scripting/introduction-saving-and-loading
 //Class was based on info from Unity saving tutorial listed above
@@ -31,7 +31,7 @@ public class SaveFile : MonoBehaviour
 	{
 		
 		//Convert RoomData Image to String
-		String RoomDataAsString = ImageToString(RoomData);
+		string RoomDataAsString = ImageToString(RoomData);
 
 		string jsonString = JsonUtility.ToJson (RoomDataAsString);
 
@@ -53,7 +53,7 @@ public class SaveFile : MonoBehaviour
 
 	//https://social.msdn.microsoft.com/Forums/en-US/011c2c24-ffc5-4be5-883b-2430267b33c8/serialize-an-image-to-a-string?forum=Vsexpressvcs
 	//Two functions below based on Solution posted on Forum listed
-	string ImageToString(Image im)
+	public static string ImageToString(Image im)
 	{
 		MemoryStream ms = new MemoryStream();
 
@@ -61,18 +61,18 @@ public class SaveFile : MonoBehaviour
 
 		byte[] array = ms.ToArray();
 
-		return Convert.ToBase64String(array);
+		return System.Convert.ToBase64String(array);
 	}
 
 	//https://social.msdn.microsoft.com/Forums/en-US/011c2c24-ffc5-4be5-883b-2430267b33c8/serialize-an-image-to-a-string?forum=Vsexpressvcs
-	public Image StringToImage(string imageString){
-		if (imageString == null)
+	public static Image StringToImage(string imageString){
+		//if (imageString == null)
 
-		throw new ArgumentNullException("imageString");
+		//throw new ArgumentNullException("imageString");
 
-		byte[] array = Convert.FromBase64String(imageString);
+		byte[] array = System.Convert.FromBase64String(imageString);
 
-		Image image = Image.FromStream(new MemoryStream(array));
+		System.Drawing.Image image = Image.FromStream(new MemoryStream(array));
 
 		return image;
 	}
