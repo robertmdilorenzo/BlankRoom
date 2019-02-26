818ae75de44ba0b95c74db48a86b7d28a66f2ef7
robertmdilorenzo <robertmdilorenzo@gmail.com>
2019-02-26 16:54:23 -0500

    New Sprite System and Tracking Fix

--- BlankRoomUnity/Assets/PenAlternate.cs
+++ BlankRoomUnity/Assets/PenAlternate.cs
@@ -8,13 +8,15 @@ public class PenAlternate : MonoBehaviour
     float distance = 5;
     ParticleSystem myTrail;
     public Transform uvLocation;
-    Vector3 uvWorldPosition;
+   // Vector3 uvWorldPosition;
     public GameObject renderPen;
     ParticleSystem renderPenParticles;
+    public Camera myCanvasCam;
+    public RenderTexture renderTexture;
     void Start()
     {
         
-        uvWorldPosition = Vector3.zero;
+        
         myTrail = gameObject.GetComponent<ParticleSystem>();
       
         renderPenParticles = renderPen.GetComponent<ParticleSystem>();
@@ -25,8 +27,9 @@ public class PenAlternate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         ObjectFollowCursor();
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0))
         {
             DoAction();
         } else if (Input.GetMouseButtonUp(0))
@@ -46,10 +49,18 @@ public class PenAlternate : MonoBehaviour
 
     void DoAction()
     {
+        
+        Vector3 uvWorldPosition = Vector3.zero;
+        canEmit(true);
         if (HitTestUVPosition(ref uvWorldPosition))
         {
-            canEmit(true);
-            
+            GameObject brush;
+            brush = (GameObject)Instantiate(Resources.Load("PaintBrushSprite"));
+            //renderPen.transform.localPosition = new Vector3(-uvWorldPosition.x, -uvWorldPosition.y);
+            //brush.transform.parent = brushContainer.transform; //Add the brush to our container to be wiped later
+            brush.transform.localPosition = uvWorldPosition; //The position of the brush (in the UVMap)
+            brush.transform.localScale = Vector3.one;//The size of the brush
+
 
         } else
         {
@@ -74,11 +85,11 @@ public class PenAlternate : MonoBehaviour
             }
             Vector2 pixelUV = new Vector2(hit.textureCoord.x, hit.textureCoord.y);
             //Debug.Log(pixelUV);
-            uvWorldPosition.x = pixelUV.x;
-            uvWorldPosition.y = pixelUV.y;
-            uvWorldPosition.z = 0.0f;
+            uvWorldPosition.x = pixelUV.x*10 - myCanvasCam.orthographicSize;
+            uvWorldPosition.y = pixelUV.y*10  - myCanvasCam.orthographicSize;
+            uvWorldPosition.z = -2.0f;
+            
             return true;
-
         }
         else
         {
@@ -88,8 +99,8 @@ public class PenAlternate : MonoBehaviour
 
     void canEmit(bool truefalse)
     {
-        myTrail.enableEmission = truefalse;
-        renderPenParticles.enableEmission = truefalse;
+        //myTrail.enableEmission = truefalse;
+       // renderPenParticles.enableEmission = truefalse;
     }
 
 
